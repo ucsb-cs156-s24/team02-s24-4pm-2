2535878322b37816a09f8d95e980ec0860200ee5
Kelly Flippo <kellyflippo@Kellys-MacBook-Pro.local>
2024-05-02 01:20:55 -0700

    formatting again

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBDatesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBDatesController.java
@@ -36,7 +36,7 @@ public class UCSBDatesController extends ApiController {
     @Autowired
     UCSBDateRepository ucsbDateRepository;
 
-    @Operation(summary= "List all ucsb dates")
+    @Operation(summary = "List all ucsb dates")
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/all")
     public Iterable<UCSBDate> allUCSBDates() {
@@ -44,7 +44,7 @@ public class UCSBDatesController extends ApiController {
         return dates;
     }
 
-    @Operation(summary= "Create a new date") 
+    @Operation(summary = "Create a new date")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/post")
     public UCSBDate postUCSBDate(
@@ -52,7 +52,6 @@ public class UCSBDatesController extends ApiController {
             @Parameter(name = "name") @RequestParam String name,
             @Parameter(name = "localDateTime") @RequestParam("localDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime localDateTime)
             throws JsonProcessingException {
-
         // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
         // See: https://www.baeldung.com/spring-date-parameters
 
@@ -67,23 +66,23 @@ public class UCSBDatesController extends ApiController {
 
         return savedUcsbDate;
     }
- 
+
     @Operation(summary = "Get a single date")
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("")
     public UCSBDate getById(
-            @Parameter(name="id") @RequestParam Long id) {
+            @Parameter(name = "id") @RequestParam Long id) {
         UCSBDate ucsbDate = ucsbDateRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException(UCSBDate.class, id));
 
         return ucsbDate;
     }
 
-    @Operation(summary= "Delete a UCSBDate")
+    @Operation(summary = "Delete a UCSBDate")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("")
     public Object deleteUCSBDate(
-            @Parameter(name="id") @RequestParam Long id) {
+            @Parameter(name = "id") @RequestParam Long id) {
         UCSBDate ucsbDate = ucsbDateRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException(UCSBDate.class, id));
 
@@ -91,11 +90,11 @@ public class UCSBDatesController extends ApiController {
         return genericMessage("UCSBDate with id %s deleted".formatted(id));
     }
 
-    @Operation(summary= "Update a single date")
+    @Operation(summary = "Update a single date")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("")
     public UCSBDate updateUCSBDate(
-            @Parameter(name="id") @RequestParam Long id,
+            @Parameter(name = "id") @RequestParam Long id,
             @RequestBody @Valid UCSBDate incoming) {
 
         UCSBDate ucsbDate = ucsbDateRepository.findById(id)
